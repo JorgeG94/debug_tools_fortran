eecd1d6d6c873c57fc0e66ad45142f4ceafb90ce
JorgeG94 <jorgegalvez1694@gmail.com>
2024-11-18 09:44:07 +1100

    make string utilities better

--- source/print/string_utilities.f90
+++ source/print/string_utilities.f90
@@ -24,44 +24,50 @@ contains
    end function concatenate_strings
 
    ! Overloaded to_string function for integer
-   function to_string_int32(i) result(str)
+   function to_string_int32(i) result(trimmed_str)
       integer(kind=int32), intent(in) :: i
       character(len=50) :: str
+      character(len=:), allocatable :: trimmed_str
       write(str, '(I0)') i  ! Convert integer to string without leading spaces
-      str = trim(str)
+      trimmed_str = trim(str)
    end function to_string_int32
-   function to_string_int64(i) result(str)
+
+   function to_string_int64(i) result(trimmed_str)
       integer(kind=int64), intent(in) :: i
       character(len=50) :: str
+      character(len=:), allocatable :: trimmed_str
       write(str, '(I0)') i  ! Convert integer to string without leading spaces
-      str = trim(str)
+      trimmed_str = trim(str)
    end function to_string_int64
 
    ! Overloaded to_string function for real
-   function to_string_dp(r) result(str)
+   function to_string_dp(r) result(trimmed_str)
       double precision, intent(in) :: r
       character(len=50) :: str
+      character(len=:), allocatable :: trimmed_str
       write(str, '(F0.12)') r  ! Convert real to string with 3 decimal places
-      str = trim(str)
+      trimmed_str = trim(str)
    end function to_string_dp
 
    ! Overloaded to_string function for character
-   function to_string_char(c) result(str)
+   function to_string_char(c) result(trimmed_str)
       character(len=*), intent(in) :: c
       character(len=500) :: str
+      character(len=:), allocatable :: trimmed_str
       str = c
-      str = trim(str)
+      trimmed_str = trim(str)
    end function to_string_char
 
-   function to_string_logical(l) result(str)
+   function to_string_logical(l) result(trimmed_str)
       logical, intent(in) :: l
       character(len=5) :: str
+      character(len=:), allocatable :: trimmed_str
       if (l) then
          str = 'TRUE'
       else
          str = 'FALSE'
       end if
-      str = trim(str)
+      trimmed_str = trim(str)
    end function to_string_logical
 
 
